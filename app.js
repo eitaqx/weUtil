89bde65792725f17d660332b05d5a651f3619856
Q <eitaqx@163.com>
2019-01-15 17:30:37 +0800

    删除不必要内容

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 //app.js
-import Utils from 'utils/util';
+import util from 'utils/util';
 import Watch from 'utils/watch';
 App({
-  util: Utils,
+  util,
   onLaunch: function () {
-    // console.log(Utils);
-    class watchCurrent extends getCurrentPages {
-      constructor(o, t){
-        super(o)
-        console.log(o, t)
-      }
-    }
-    new watchCurrent('123')
-    Utils.getPosition(res => {
-      console.log(res)
-    })
+    
   },
   globalData: {
     userInfo: null
